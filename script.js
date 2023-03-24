e36dc182407536e7446ca6a0981695d02089c597
Mohamed Fazil M <fazilmm860@gmail.com>
2023-03-24 07:17:18 +0530

    querySelectors of Class are saved in Variables for easy to use

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'use strict';
+const model = document.querySelector('.model');
+const overlay = document.querySelector('.overlay');
+const btnCloseModel = document.querySelector('.close-modal');
+const btnShowModal = document.querySelectorAll('.show-modal');
+console.log(btnShowModal);
+for (let i = 0; i < btnShowModal.length; i++)
+    console.log(btnShowModal[i].textContent);
