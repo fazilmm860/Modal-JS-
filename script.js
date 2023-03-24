63e2315105eeedeb7bd64cb855c82be424483aad
Mohamed Fazil M <fazilmm860@gmail.com>
2023-03-24 17:05:25 +0530

    Modal project 1 section is done

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 'use strict';
-const model = document.querySelector('.model');
+const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModel = document.querySelector('.close-modal');
 const btnShowModal = document.querySelectorAll('.show-modal');
 console.log(btnShowModal);
+const openModal = function () {
+    console.log('Button Clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden')
+}
+const clossModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden')
+}
 for (let i = 0; i < btnShowModal.length; i++)
-    console.log(btnShowModal[i].textContent);
+    btnShowModal[i].addEventListener('click', openModal);
+
+
+
+btnCloseModel.addEventListener('click', clossModal)
+overlay.addEventListener('click', clossModal)
